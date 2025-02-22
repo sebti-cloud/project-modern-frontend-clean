da573162876fcd4bdb629e553a5fb70f77056c03
Mohamed Lakhal <lahrechsimo732@gmail.com>
2025-02-22 13:11:35 +0100

    Correction vercel.json et config frontend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/app.css';
 import App from './App';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,7 +15,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/app.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import App from './App';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
